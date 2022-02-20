fcdb480fa69c7428b75b075af00cefd16b2cfd35
julietvt <freshitpt@gmail.com>
2022-02-20 10:33:37 +0200

    user cards

--- src/App.js
+++ src/App.js
@@ -1,93 +1,82 @@
-import './App.css';
+import styles from './App.module.css';
 import React, { Component, Fragment } from 'react';
-import CounterDisplayValue from './components/CounterDisplayValue';
+import ColorHash from 'color-hash';
+import Icon from '@mdi/react';
+import { mdiCheck } from '@mdi/js';
+import classNames from 'classnames';
+import _ from 'lodash';
 
-/*
-Props && State только для чтения!!!!!
-имутальбальный, неизменяемое
+const users = [];
 
-аналог чистых функций
+const SelectedList = (props) => {
+  const { users } = props;
+  return (
+    <ul style={{ textAlign: 'left' }}>
+      {users.map((item, index, arr) => (
+        <li className={styles.selectedItem}>{`${item.name} ${item.surname} ${
+          index == arr.length - 1 ? '' : ','
+        }`}</li>
+      ))}
+    </ul>
+  );
+};
 
-setState запрос на измение. итог его - создание нового состояния, замена (аналог http-запроса)
+const User = (props) => {
+  const { user, select, index } = props;
 
--------------------------
-setState
-
-нельзя this.state.count = 10;
-
-1 this.setState({ count: this.state.count + 1 })]
-
- - this.setState({ count: ++count })
-
-2 this.setState ( (state,props) => ({ count: state.count + props.step }) );
-
- - this.setState ( { count: this.state.count + this.props.step } );
-
-*/
-
-/*
-
-Передача данных в реакт
-
-1 Родитель (state:count) -> Ребенок
-
-parent <Child count={this.state.count} />
-child  this.props.count
-
-2 Ребенок (установить новое состояние родителя count ) -> Родитель (state:count)
-через callback
-
-parent:
-
-1
- updateCount = (value) => {
-  this.setState({count: value})
-}
-
-2 
-<Child updateCount={this.updateCount} />
-
-child:
-есть свое состояние score установить как состояние state для родителя
-
-
-this.props.updateCount(this.state.score)
-
-
-3 Ребенок 1 => Ребенок2
-только через родителя
-
-1 сп (совместное использование) поднять состояние до ближайшего родителя
-
-2 сп 1 данные от ребенка1 к родителя через колбэк 2 родитель через пропсы передает данные ребенку2
-
-
-*/
-
+  const onSelect = () => {
+    select(index);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.userImageWrapper}>
+        <img className={styles.userImage} src={user.imageSrc} alt="avatar" />
+      </div>
+      <div className={styles.userInfoWrapper}>
+        <div className={styles.fullname}>{`${user.name} ${user.surname}`}</div>
+        <div className={styles.info}>Level {user.level}</div>
+      </div>
+      <div
+        onClick={onSelect}
+        className={classNames(styles.check, {
+          [styles.checked]: user.isSelected,
+        })}
+      >
+        <Icon path={mdiCheck} color={'white'} />
+      </div>
+    </div>
+  );
+};
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      items: users.map((u) => ({ ...u, isSelected: false })),
     };
-    console.log('this constructor', this);
-    this.inc = this.inc.bind(this);
   }
-  componentDidMount() {}
-  componentDidUpdate(prevProps, prevState, snapshot) {}
 
-  inc() {
-    // this.state.count = this.state.count + 1; ошибка!!!
-    console.log('this function ', this);
-    return this.setState({ count: this.state.count + 1 });
-  }
+  select = (index) => {
+    const newItems = _.clone(this.state.items);
+    newItems[index].isSelected = !newItems[index.isSelected];
+    this.setState({ items: newItems });
+  };
 
   render() {
+    const { items } = this.state;
     return (
-      <Fragment>
-        <CounterDisplayValue count={this.state.count} />
-        <button onClick={this.inc}>+</button>
-      </Fragment>
+      <ul className={styles.container}>
+        {
+          <li className={styles.userCardContainer}>
+            <span className={styles.info}>To: </span>
+            <SelectedList users={items.filter((item) => item.isSelected)} />
+          </li>
+        }
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <User index={index} select={this.select} user={item} />
+          </li>
+        ))}
+      </ul>
     );
   }
 }
